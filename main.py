a409bede2e11c5f15741f5ae98e8358f1207c421
James Shelley <james.shelley@gmail.com>
2026-01-11 16:49:26 -0500

    uploaded

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-radius = float(input("Enter the radius: "))
-pi = 3.14159
-circumference = 2 * pi * radius
-area = pi * radius ** 2
+while True:
+    try:
+        radius = float(input("Enter the radius: "))
+        break  # Exit loop if conversion succeeds
+    except ValueError:
+        print("Invalid input! Please enter a number.")
+
+circumference = 2 * 3.14 * radius
+area = 3.14 * radius ** 2
+
 print("Circumference:", circumference)
 print("Area:", area)
